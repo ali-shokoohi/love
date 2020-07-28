3202ef44a56abb51b941899fe0ae1cf82416634c
Ali Shokoohi <ali@shokoohi.dev>
2020-07-28 13:40:19 +0430

    Updates

---
+++ precache-manifest.ec2be62990dfa03c90c5863a002cb581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98795c8d33590f5f6bca598132290d46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a81a00ca3cab71b364f3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34276791967cbcc2909b",
+    "url": "/static/js/2.bd66bc7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd66bc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81a00ca3cab71b364f3",
+    "url": "/static/js/main.70882538.chunk.js"
+  },
+  {
+    "revision": "e003d41a3b0308c15027",
+    "url": "/static/js/runtime-main.6c28a8cd.js"
+  }
+]);
